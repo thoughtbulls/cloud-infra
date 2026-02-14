d508d063d4ae3de7d9a8f7099332f8737331a0d6
Narendra Kashyap <thoughtbullswork@gmail.com>
2026-02-14 11:22:23 +0530

    Initial cloud-infra structure

---
+++ infra/databricks-workspace/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.30"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "databricks" {
+  host = "https://accounts.cloud.databricks.com"
+}
---
+++ infra/storage/output.tf
@@ -0,0 +1,3 @@
+output "databricks_root_bucket" {
+  value = aws_s3_bucket.databricks_root.bucket
+}
